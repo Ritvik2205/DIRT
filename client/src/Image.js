07ef11e5fe06865b7a629916a151aa4eb273660d
IainWt <weetmaniain@gmail.com>
2024-02-03 18:08:20 +0000

    Image

--- client/src/Image.js
+++ client/src/Image.js
@@ -1,28 +1,25 @@
 export default function Image(requestData) {
   async function fetchURL(requestData) {
-    try {
-      const response = fetch("http://127.0.0.1:8000/image/", {
+    fetch("http://127.0.0.1:8000/image/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: {
-          "prompt": JSON.stringify(requestData),
-        },
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const data = await response.json();
+    })
+    .then(response => response.json())
+    .then(data => {
       console.log("URL from backend:", data); // 'data.url' should be replaced with the actual key used in the JSON response
-      return data.url;
-    } catch (error) {
+      return data["url"];
+    })
+    .catch (error => {
       console.error("Error fetching URL from backend:", error);
       return null;
-    }
+    })
   }
 
   fetchURL(requestData);
 }
+
+// if (!response.ok) {
+//   throw new Error(`HTTP error! status: ${response.status}`);
+// }
